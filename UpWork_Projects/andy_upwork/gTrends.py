fa57c1b8041d7f3a3d66852fd91329e3a91a3eb3
byom26 <p.byom26@gmail.com>
2020-07-06 18:38:54 +0530

    fixed issues in milestone2 spiders

--- UpWork_Projects/andy_upwork/gTrends.py
+++ UpWork_Projects/andy_upwork/gTrends.py
@@ -3,10 +3,12 @@ import pandas as pd
 from datetime import date, datetime
 
 
-pytrend = TrendReq()
+pytrend = TrendReq(timeout=(10,20), retries=3)
 pytrend.build_payload(
-    kw_list=["Corona", "Elections", "China", "Trump"],
-    timeframe='today 1-m'
+    kw_list=["ps4", "ps5", "laptop", "samsung"],
+    timeframe='today 1-m',
+    geo='',
+    gprop=''
 )
 
 df = pytrend.interest_over_time()
