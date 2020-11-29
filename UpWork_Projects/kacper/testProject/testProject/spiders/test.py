43a2d1f26197a4f825fbc42a89656c8997a13518
byom26 <p.byom26@gmail.com>
2020-11-29 09:53:19 +0530

    added new scraper for iherb

--- UpWork_Projects/kacper/testProject/testProject/spiders/test.py
+++ UpWork_Projects/kacper/testProject/testProject/spiders/test.py
@@ -13,7 +13,8 @@ class TestSpider(scrapy.Spider):
 
     def start_requests(self):
         yield SeleniumRequest(
-            url="https://www.sustainalytics.com/esg-ratings/?currentpage=1",
+            #url="https://www.sustainalytics.com/esg-ratings/?currentpage=1",
+            url="https://www.udr.com/inland-empire-apartments/rancho-cucamonga/verano-at-rancho-cucamonga-town-square/amenities",
             wait_time=10,
             callback=self.parse
         )
@@ -21,31 +22,56 @@ class TestSpider(scrapy.Spider):
     def parse(self, response):
         driver = response.meta['driver']
         driver.maximize_window()
-        driver.execute_script("window.open('');")
-        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(3)
 
-        for i in range(2,436):
-            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'results mt-5')]/div/div/a")))
-            time.sleep(2)
-            html = driver.page_source
-            respObj = Selector(text=html)
+        html6 = driver.page_source
+        respObj6 = Selector(text=html6)
+        amntys = respObj6.xpath("//section/div[@class='row']")
+        for amnty in amntys:
+            chkCmty = amnty.xpath(".//span[contains(text(), 'Community Amenities')]/text()").get()
+            chkApt = amnty.xpath(".//span[contains(text(), 'Apartment Amenities')]/text()").get()
+            if chkCmty :
+                aptAmnty = amnty.xpath(".//ul[contains(@class, 'amenities-list')]/li/text()").getall()
+                aptAmntyF = "|".join(x.strip() for x in aptAmnty)
+            elif chkApt :
+                cmntyAmnty = amnty.xpath(".//ul[contains(@class, 'amenities-list')]/li/text()").getall()
+                cmntyAmntyF = "|".join(y.strip() for y in cmntyAmnty)
+            else:
+                aptAmntyF = None
+                cmntyAmntyF = None
+            chkCmty = None
+            chkApt = None
+        yield{
+            'aptAmntyF': aptAmntyF,
+            'cmntyAmntyF': cmntyAmntyF,
+        }
 
-            driver.switch_to.window(driver.window_handles[1])
-            companies = respObj.xpath("//div[contains(@class, 'results mt-5')]/div/div/a")
-            for company in companies:
-                url = company.xpath(".//@href").get()
-                driver.get(f"https://www.sustainalytics.com{url.replace('..', '')}")
-                html1 = driver.page_source
-                respObj1 = Selector(text=html1)
-                yield{
-                    'Identifier': respObj1.xpath("normalize-space(//p[contains(text(), 'Identifier')]/strong/text())").get(),
-                    'Name': respObj1.xpath("normalize-space(//div[contains(@class, 'company-name')]/div/h2/text())").get(),
-                    'Country': respObj1.xpath("normalize-space(//p[contains(text(), 'Country')]/strong/text())").get(),
-                    'Industry': respObj1.xpath("normalize-space(//p[contains(text(), 'Industry')]/strong/text())").get(),
-                    'ESG score': respObj1.xpath("normalize-space(//div[contains(@class, 'risk-rating-score')]/span/text())").get(),
-                    'Industry rank': respObj1.xpath("normalize-space(//strong[@class='industry-group-position']/text())").get(),
-                    'Global rank': respObj1.xpath("normalize-space(//strong[@class='universe-position']/text())").get()
-                }
-            driver.switch_to.window(driver.window_handles[0])
 
-            driver.get(f"https://www.sustainalytics.com/esg-ratings/?currentpage={i}")
+        # driver.execute_script("window.open('');")
+        # driver.switch_to.window(driver.window_handles[0])
+
+        # for i in range(2,436):
+        #     WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(@class, 'results mt-5')]/div/div/a")))
+        #     time.sleep(2)
+        #     html = driver.page_source
+        #     respObj = Selector(text=html)
+
+        #     driver.switch_to.window(driver.window_handles[1])
+        #     companies = respObj.xpath("//div[contains(@class, 'results mt-5')]/div/div/a")
+        #     for company in companies:
+        #         url = company.xpath(".//@href").get()
+        #         driver.get(f"https://www.sustainalytics.com{url.replace('..', '')}")
+        #         html1 = driver.page_source
+        #         respObj1 = Selector(text=html1)
+        #         yield{
+        #             'Identifier': respObj1.xpath("normalize-space(//p[contains(text(), 'Identifier')]/strong/text())").get(),
+        #             'Name': respObj1.xpath("normalize-space(//div[contains(@class, 'company-name')]/div/h2/text())").get(),
+        #             'Country': respObj1.xpath("normalize-space(//p[contains(text(), 'Country')]/strong/text())").get(),
+        #             'Industry': respObj1.xpath("normalize-space(//p[contains(text(), 'Industry')]/strong/text())").get(),
+        #             'ESG score': respObj1.xpath("normalize-space(//div[contains(@class, 'risk-rating-score')]/span/text())").get(),
+        #             'Industry rank': respObj1.xpath("normalize-space(//strong[@class='industry-group-position']/text())").get(),
+        #             'Global rank': respObj1.xpath("normalize-space(//strong[@class='universe-position']/text())").get()
+        #         }
+        #     driver.switch_to.window(driver.window_handles[0])
+
+        #     driver.get(f"https://www.sustainalytics.com/esg-ratings/?currentpage={i}")
