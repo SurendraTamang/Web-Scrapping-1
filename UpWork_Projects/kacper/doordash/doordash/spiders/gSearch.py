fc8f5e75928aef235b97630ab0ba18751e1df42f
byom26 <p.byom26@gmail.com>
2021-01-31 16:18:30 +0530

    updated gSearch script

--- UpWork_Projects/kacper/doordash/doordash/spiders/gSearch.py
+++ UpWork_Projects/kacper/doordash/doordash/spiders/gSearch.py
@@ -7,12 +7,22 @@ from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 import time
 import pandas as pd
+from urllib.parse import unquote
+import random
 
 
 class GsearchSpider(scrapy.Spider):
     name = 'gSearch'
 
-    df = pd.read_excel("./data/doordashData-phase1.xlsx")
+    df = pd.read_excel("./splitData.xlsx")
+    cntr = 1
+
+    def extractUrl(self, rawUrl):
+        try:
+            url = rawUrl.split("&url=")[-1].split("&")[0]
+            return unquote(url).split("?utm_source=")[0]
+        except:
+            return None
     
     def start_requests(self):
         yield SeleniumRequest(
@@ -35,23 +45,46 @@ class GsearchSpider(scrapy.Spider):
 
         driver.maximize_window()
 
-        WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='g']")))
+        for _,val in self.df.iterrows():
+            #try:
+            self.cntr += 1
+            if self.cntr % 14 == 0:
+                driver.refresh()
+                time.sleep(random.randint(5,10))
+            WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='g']")))
 
-        #   SEARCHING FOR THE QUERY STRING  #
-        inputElem = driver.find_element_by_xpath("//input[@title='Search']")
-        inputElem.clear()
-        inputElem.send_keys("It's Just Wings,Alamogordo,Albuquerque")
+            #   SEARCHING FOR THE QUERY STRING  #
+            inputElem = driver.find_element_by_xpath("//input[@title='Search']")
+            inputElem.clear()
+            inputElem.send_keys(val['gSearchQuery'])
 
-        serachBtnElem = driver.find_element_by_xpath("//button[@type='submit']")
-        driver.execute_script("arguments[0].click()", serachBtnElem)
+            serachBtnElem = driver.find_element_by_xpath("//button[@type='submit']")
+            driver.execute_script("arguments[0].click()", serachBtnElem)
 
-        time.sleep(1)
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='g']")))
+            time.sleep(1)
+            WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='g']")))
 
-        html = driver.page_source
-        respObj = Selector(text=html)
+            html = driver.page_source
+            respObj = Selector(text=html)
 
-        yield {
-            'Website': respObj.xpath("//div[text()='Website']/parent::a/@href").get(),
-            'Phone': respObj.xpath("normalize-space(//span[contains(@aria-label, 'phone number')]/text())").get()
-        }
+            website = respObj.xpath("//div[text()='Website']/parent::a/@href").get()
+            phone = respObj.xpath("normalize-space(//span[contains(@aria-label, 'phone number')]/text())").get()
+            if not website:
+                website = respObj.xpath("//div[text()='Website']/parent::div/parent::a/@href").get()
+            if not phone:
+                phone = respObj.xpath("normalize-space((//div[text()='Website']/parent::div/parent::a/preceding-sibling::a)[1]//span[contains(@class,'details')]/div[2]/span[last()]/text())").get()
+            if not phone:    
+                phone = respObj.xpath("normalize-space((//div[text()='Website']/parent::div/parent::a/preceding-sibling::a)[1]//span[contains(@class,'details')]/div[3]/span[last()]/text())").get()
+            yield {
+                'Id': val['Id'],
+                'City': val['State'],
+                'Area': val['City'],
+                'Restaurant Name': val['Restaurant name'],
+                'Average Rating': val['Average rating'],
+                'Number Of Reviews': val['Number of reviews'],
+                'Search Queries': val['gSearchQuery'],
+                'Website': self.extractUrl(website),
+                'Phone': phone
+            }
+            # except:
+            #     pass
