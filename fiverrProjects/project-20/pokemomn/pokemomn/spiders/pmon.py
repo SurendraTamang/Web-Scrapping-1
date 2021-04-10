929bcc549d4802de9c4a9622879ae12d6d78c20d
byom26 <p.byom26@gmail.com>
2021-04-10 18:00:25 +0530

    updated fiverr - project20 scraper

--- fiverrProjects/project-20/pokemomn/pokemomn/spiders/pmon.py
+++ fiverrProjects/project-20/pokemomn/pokemomn/spiders/pmon.py
@@ -8,6 +8,8 @@ class PmonSpider(scrapy.Spider):
         yield scrapy.Request(
             url="https://www.plazajapan.com/pokemon-center-original/",
             callback=self.web1_prod_listings
+            # url="https://www.plazajapan.com/4521329327402/",
+            # callback=self.web1_prod_details
         )
 
     def web1_prod_listings(self, response):
@@ -28,28 +30,19 @@ class PmonSpider(scrapy.Spider):
 
     def web1_prod_details(self, response):
         upc = response.url.split("/")[-2]
-        stock = "Out of stock"
-        if response.xpath("//input[contains(@id, 'form-action-buyNow')]").get():
-            stock = "In Stock"
 
-        if stock != "Out of stock":
+        if response.xpath("//input[contains(@id, 'form-action-buyNow')]").get():
             yield scrapy.Request(
                 url=f"https://www.pokemoncenter-online.com/?p_cd={upc}",
                 callback=self.web2_stock_status,
                 meta={
                     'upc': upc,
-                    'stock': stock
                 }            
             )
 
     def web2_stock_status(self, response):
-        stock1 = response.request.meta['stock']
         upc = response.request.meta['upc']
-        stock2 = "Out of stock"
-        if response.xpath("//td/img[contains(@src, 'btn_cart.jpg')]").get():
-            stock2 = "In Stock"
-
-        yield{
-            'Upc': upc,
-            'Stock': stock2,
-        }
+        if not response.xpath("//td/img[contains(@src, 'btn_cart.jpg')]").get():
+            yield{
+                'Upc': upc
+            }
